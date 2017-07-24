5c7bf5b32101198b48a99b701ce5aa0c916a03a4
James Canning <james@brudil.com>
2017-07-24 15:46:27 +0100

    Add author filtering

--- lowdown/manage/content/views.py
+++ lowdown/manage/content/views.py
@@ -1,9 +1,8 @@
 import json
-
 import django_filters
 from django.db import transaction
-
 from lowdown.core.content.models import Content, ContentRevision, ContentEditorialMetadata
+from lowdown.core.authors.models import Author
 from lowdown.manage.content.serializers import ContentEditorialMetadataSerializer, ContentRevisionSerializer, \
     ContentRevisionLocalSerializer
 from lowdown.core.topics.models import Topic
@@ -21,6 +20,7 @@ class ContentListFilter(filters.FilterSet):
     tone = django_filters.NumberFilter(name='current_revision__tone')
     topics = django_filters.ModelMultipleChoiceFilter(name='current_revision__topics', queryset=Topic.objects.all())
     series = django_filters.ModelChoiceFilter(name='current_revision__series', queryset=LowdownUser.objects.all())
+    authors = django_filters.ModelChoiceFilter(name='current_revision__authors', queryset=Author.objects.all())
 
     class Meta:
         model = ContentEditorialMetadata
